53dd52584549771a116b3d6a5f2258a90d68c9b4
gssgit <hatsanyuks@gmail.com>
2013-01-31 14:49:46 +0200

    fixed main page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,63 +1,32 @@
 class TasksController < ApplicationController
-  # GET /tasks
-  # GET /tasks.json
+
+  before_filter :find_task, except: [:index, :new]
+
   def index
     @tasks = Task.all
     @comments = Comment.all
-
-
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tasks }
-    end
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
-    @task = Task.find(params[:id])
-    @comments = Task.find(params[:id]).comments
-    if User.find_by_id(@task.user_id)
-      @user_title = User.find_by_id(@task.user_id).title
-    end
-    if Group.find_by_id(@task.group_id)
-      @group_title = Group.find_by_id(@task.group_id).title
-    end
+    @comments = @task.comments
+    @user_title = @task.user.title if @task.user
+    @group_title = @task.group.title if @task.group
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @task }
+      format.js
     end
   end
 
-  # GET /tasks/new
-  # GET /tasks/new.json
   def new
     @task = Task.new
-    #@user = User.select(:title)
-    #raise User.select(:title).inspect
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @task }
-    end
   end
 
-  # GET /tasks/1/edit
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  # POST /tasks
-  # POST /tasks.json
   def create
     new_order = lambda {|g,t,u| Order.create(:group_id => g, :task_id => t, :user_id => u)}
     @task = Task.new(params[:task])
 
     respond_to do |format|
-
-
     if @task.save
       if (@task.group_id && @task.user_id)
         new_order.call(@task.group_id, @task.id, nil)
@@ -75,20 +44,9 @@ class TasksController < ApplicationController
     end
   end
 
-
-
-
-
-
   end
 
-  # PUT /tasks/1
-  # PUT /tasks/1.json
   def update
-    @task = Task.find(params[:id])
-
-
-
     respond_to do |format|
       if @task.update_attributes(params[:task])
 
@@ -101,15 +59,18 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     respond_to do |format|
       format.html { redirect_to tasks_url }
-      format.json { head :no_content }
+      format.js
     end
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
